aa8312adc39eb86f45bfa4921e3ab57cf8aa42bb
afablelorenz <afablelorenz@gmail.com>
2024-06-14 17:15:05 +0200

    update

---
+++ src/components/MainContent.vue
@@ -0,0 +1,55 @@
+<script>
+import axios from 'axios';
+export default{
+    components:{
+
+    },
+    data(){
+        return{
+            filmList : [],
+            filmApiUrl : 'https://api.themoviedb.org/3/search/movie',
+            filmListSearch : 'star',
+            filmApiKey : '0d49198008396e97d715e909d25476c0',
+        }
+    },
+    methods:{
+        getFilmList(){
+            axios.get( this.filmApiUrl, {
+                params: {
+                    api_key:this.filmApiKey,
+                    query:this.filmListSearch,
+                    page:1
+            },
+            })
+            .then(function (response) {
+                console.log('entrato');
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
+            })  
+        }
+    },
+    created(){
+        this.getFilmList();
+    }
+}
+
+</script>
+
+<template>
+<main>
+    <input>
+    <button>
+
+    </button>
+</main>
+
+</template>
+
+<style>
+
+</style>
