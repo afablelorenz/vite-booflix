c10926efa9f3c2664c8fd029f91cf48115e46ee0
afablelorenz <afablelorenz@gmail.com>
2024-06-18 17:46:34 +0200

    update

--- src/components/FilmItem.vue
+++ src/components/FilmItem.vue
@@ -11,8 +11,8 @@
         }
     },
     computed: {
-        starRating(){
-            
+        starRating() {
+            return Math.ceil(this.film.vote_average / 2);
         }
     },
 }
@@ -33,9 +33,17 @@
     <p>
         Lingua: <span class="lang-icon" :class="'lang-icon-' + film.original_language"></span>
     </p>
+    <p>
+        Voto: 
+        <span v-for="n in 5" :key="n" class="star">
+            <font-awesome-icon :icon="n <= starRating ? 'fas fa-star' : 'far fa-star'" />
+        </span>
+    </p>
 </div>
 </template>
 
 <style>
-
+.star {
+    color: gold;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faStar as fasFaStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as farFaStar } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-createApp(App).mount('#app')
+library.add(fasFaStar, farFaStar);
+const app = createApp(App);
+
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.mount('#app');
